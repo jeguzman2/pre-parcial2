2f3ee81769c38cb648b35f0843b43f63b5c564d0
jeguzman2 <je.guzman2@uniandes.edu.co>
2026-05-12 17:45:00 -0500

    controller implementado de travel-plans

--- src/travel-plans/travel-plans.controller.ts
+++ src/travel-plans/travel-plans.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+
+import { TravelPlansService } from './travel-plans.service';
+import { CreateTravelPlanDto } from './dto/create-travel-plan.dto';
 
 @Controller('travel-plans')
-export class TravelPlansController {}
+export class TravelPlansController {
+  constructor(
+    private readonly travelPlansService: TravelPlansService,
+  ) {}
+
+  @Post()
+  async create(
+    @Body() createTravelPlanDto: CreateTravelPlanDto,
+  ) {
+    return await this.travelPlansService.create(
+      createTravelPlanDto,
+    );
+  }
+
+  @Get()
+  async findAll() {
+    return await this.travelPlansService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return await this.travelPlansService.findOne(id);
+  }
+
+  @Delete(':id')
+  async remove(
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    await this.travelPlansService.remove(id);
+
+    return {
+      message: `Travel plan with ID ${id} deleted successfully`,
+    };
+  }
+}
